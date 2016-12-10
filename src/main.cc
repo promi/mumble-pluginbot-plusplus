e96d6a90a4637a843f90bc0bc0b503ad207b453d
Promi <prometheus@unterderbruecke.de>
2016-12-10 15:38:01 +0100

    Correctly handle NULL return of getenv ()

--- src/main.cc
+++ src/main.cc
@@ -132,26 +132,40 @@ parse_cmd_options (int argc, char *argv[], MumblePluginBot::Settings &settings)
   return config_filename;
 }
 
+std::string
+getenv_nullsafe (const std::string &name)
+{
+  const char* value = getenv (name.c_str ());
+  if (value == nullptr)
+    {
+      return std::string {};
+    }
+  else
+    {
+      return std::string {value};
+    }
+}
+
 void
 set_dirs (MumblePluginBot::Settings settings)
 {
   // https://standards.freedesktop.org/basedir-spec/basedir-spec-0.8.html
-  fs::path home = getenv("HOME");
+  fs::path home = getenv_nullsafe ("HOME");
   assert (home != "");
-  fs::path xdg_data_home = getenv ("XDG_DATA_HOME");
+  fs::path xdg_data_home = getenv_nullsafe ("XDG_DATA_HOME");
   if (xdg_data_home == "" || !xdg_data_home.is_absolute ())
     {
       xdg_data_home = home / ".local" / "share";
     }
   assert (xdg_data_home != "");
-  fs::path xdg_runtime_dir = getenv ("XDG_RUNTIME_DIR");
+  fs::path xdg_runtime_dir = getenv_nullsafe ("XDG_RUNTIME_DIR");
   if (xdg_runtime_dir == "" || !xdg_runtime_dir.is_absolute ())
     {
       std::cerr << "warning: XDG_RUNTIME_DIR is not set" << std::endl;
       xdg_runtime_dir = home / ".config";
     }
   assert (xdg_runtime_dir != "");
-  fs::path xdg_config_home = getenv ("XDG_CONFIG_HOME");
+  fs::path xdg_config_home = getenv_nullsafe ("XDG_CONFIG_HOME");
   if (xdg_config_home == "" || !xdg_config_home.is_absolute ())
     {
       xdg_config_home = home / ".config";

3772840d7a772d2fc2c76aeed05708265da37b2c
Promi <prometheus@unterderbruecke.de>
2016-12-10 15:38:47 +0100

    Undo PIMPL, doesn't make sense for Settings

--- src/pluginbot/settings.hh
+++ src/pluginbot/settings.hh
@@ -26,21 +26,108 @@
 #include <list>
 #include <memory>
 
-#include "pluginbot/setting-descriptor.hh"
-
 namespace MumblePluginBot
 {
   class Settings
   {
   public:
     Settings ();
-    Settings (const Settings &other);
-    Settings (Settings &&other);
-    Settings& operator= (const Settings &other);
-    Settings&& operator= (Settings &&other);
-    const std::list<SettingDescriptor>& descriptors () const;
-  private:
-    struct Impl;
-    std::unique_ptr<Impl> pimpl;
+
+    std::string version = "0.1";
+    std::string main_tempdir = "temp";
+    bool ducking = false;
+    uint ducking_vol = 20;
+    bool control_automute = true;
+    uint16_t chan_notify = 0x0000;
+    std::string controlstring = ".";
+    bool debug = true;
+    bool verbose = true;
+    uint ticks_per_hour = 3600;
+    bool listen_to_private_message_only = false;
+    bool listen_to_registered_users_only = true;
+    bool use_vbr = true;
+    bool stop_on_unregistered_users = true;
+    bool use_comment_for_status_display = true;
+    bool set_comment_available = false;
+    // Blacklist entry: user_hash=username
+    std::map<std::string, std::string> blacklist;
+    struct
+    {
+      std::string host = "127.0.0.1";
+      uint16_t port = 64738;
+      std::string username = "MumblePluginbotPlusPlus";
+      std::string userpassword = "";
+      std::string targetchannel = "";
+    } connection;
+    // TODO: This really belongs to libmumble-pluginbot-plusplus-mumble!
+    struct
+    {
+      bool allow_html = false;
+      uint32_t message_length = 0;
+      uint32_t image_message_length = 0;
+    } server_config;
+    // TODO: This really belongs to libmumble-pluginbot-plusplus-mumble!
+    struct
+    {
+      std::string version;
+      bool positional;
+      bool push_to_talk;
+    } suggest_config;
+    std::string logo;
+    std::string superanswer;
+    struct
+    {
+      std::string fifopath = "mpd.fifo";
+      std::string host = "localhost";
+      uint16_t port = 7701;
+    } mpd;
+    std::string certdir = "certs";
+    uint quality_bitrate = 72000;
+    uint initial_volume = 65;
+    bool controllable = true;
+    bool need_binding = false;
+    std::string boundto = "";
+    std::string superpassword = "kaguBe gave me all the power from kaguBe and I wish to ";
+    /*
+    youtube_downloadsubdir", "downloadedfromyt"},
+    youtube_tempsubdir", "youtubeplugin"},
+    youtube_stream", "nil"},
+    youtube_youtubedl", "youtube-dl"},
+    youtube_to_mp3", "nil"},
+    youtube_youtubedl_options", ""},
+    youtube_commandlineprefixes", ""},
+    youtube_maxresults", "200"},
+    soundcloud_downloadsubdir", "downloadedfromsc"},
+    soundcloud_tempsubdir", "soundcloudplugin"},
+    soundcloud_youtubedl", "youtube-dl"},
+    soundcloud_to_mp3", "nil"},
+    soundcloud_youtubedl_options", ""},
+    soundcloud_commandlineprefixes", ""},
+    bandcamp_downloadsubdir", "downloadedfrombc"},
+    bandcamp_tempsubdir", "bandcampplugin"},
+    bandcamp_youtubedl", "youtube-dl"},
+    bandcamp_to_mp3", "nil"},
+    bandcamp_youtubedl_options", ""},
+    bandcamp_commandlineprefixes", ""},
+    ektoplazm_downloadsubdir", "ektoplazm"},
+    ektoplazm_tempsubdir", "ektoplazmplugin"},
+    mpd_musicfolder", "music"},
+    control_historysize", "20"},
+      {
+        "mpd_template_comment_disabled",
+        "<b>Artist: </b>DISABLED<br />"
+        "<b>Title: </b>DISABLED<br />"
+        "<b>Album: </b>DISABLED<br /><br />"
+        "<b>Write %shelp to me, to get a list of my commands!"
+      },
+      {
+        "mpd_template_comment_enabled",
+        "<b>Artist: </b>%s<br />"
+        "<b>Title: </b>%s<br />" \
+        "<b>Album: </b>%s<br /><br />" \
+        "<b>Write %shelp to me, to get a list of my commands!</b>"
+      }
+    };
+*/
   };
 }

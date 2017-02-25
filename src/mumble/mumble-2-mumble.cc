0a4982f55ef1efab7d72dfd6b86b2fa7f8ce9ae3
Promi <prometheus@unterderbruecke.de>
2017-02-25 21:54:06 +0100

    Apply PIMPL to Mumble2Mumble

--- src/mumble/mumble-2-mumble.cc
+++ src/mumble/mumble-2-mumble.cc
@@ -21,51 +21,67 @@
     You should have received a copy of the GNU Affero General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
-#include <algorithm>
-#include <iostream>
-#include <chrono>
-
 #include "mumble/mumble-2-mumble.hh"
+
+#include <algorithm>
+#include <chrono>
+#include <iostream>
+#include <map>
+#include <mutex>
+#include <queue>
+#include <thread>
+
+#include "opus/decoder.hh"
+#include "opus/encoder.hh"
 #include "mumble/udp-packet.hh"
 
 namespace Mumble
 {
-  Mumble2Mumble::Mumble2Mumble (const Aither::Log &log, Codec codec,
-                                Connection &conn,
-                                size_t sample_rate,
-                                size_t frame_size, size_t channels, size_t bitrate):
-    m_log (log), m_codec (codec), m_conn (conn), m_sample_rate (sample_rate),
-    m_channels (channels),
-    m_opus_encoder (m_log, static_cast<Opus::SampleRate> (sample_rate),
-                    sample_rate / 100,
-                    static_cast<Opus::Channels> (channels), COMPRESSED_SIZE)
+  struct Mumble2Mumble::Impl
+  {
+    const Aither::Log &m_log;
+    PacketDataStream m_pds;
+    PacketDataStream m_sendpds;
+    Codec m_codec;
+    Connection &m_conn;
+    int m_sample_rate;
+    int m_channels;
+    std::mutex m_pds_lock;
+    std::map<uint32_t, Opus::Decoder> m_opus_decoders;
+    // std::map<uint32_t, Celt::Decoder> m_celt_decoders;
+    std::map<uint32_t, std::queue<std::vector<int16_t>>> m_queues;
+    Opus::Encoder m_opus_encoder;
+    // Celt::Encoder m_celt_encoder;
+    std::vector<int16_t> m_rawaudio;
+    uint32_t m_seq = 0;
+    std::queue<std::vector<uint8_t>> m_plqueue;
+    std::thread m_consume;
+    // bool m_consume_running;
+    Impl (const Aither::Log &log, Codec codec, Connection &conn, size_t sample_rate, size_t channels, size_t bitrate)
+      : m_log (log), m_codec (codec), m_conn (conn), m_sample_rate (sample_rate), m_channels (channels), m_opus_encoder (m_log, static_cast<Opus::SampleRate> (sample_rate), sample_rate / 100, static_cast<Opus::Channels> (channels), COMPRESSED_SIZE)
 /*
 m_celt_encoder (sample_rate, sample_rate / 100, channels, [bitrate / 6800, 127].min)
 */
-  {
-    // TODO: Add other allowed values for opus
-    assert (sample_rate == 48'000 /*'*/);
-    assert (channels == 1 || channels == 2);
-    (void) frame_size;
-    // CBR
-    m_opus_encoder.vbr (false);
-    m_opus_encoder.bitrate (bitrate);
-    m_opus_encoder.signal (Opus::Signal::music);
-    //m_celt_encoder.vbr_rate = bitrate
-    //m_celt_encoder.prediction_request = 0
-    m_consume = std::thread {[this] ()
     {
-      this->consume();
+      // TODO: Add other allowed values for opus
+      assert (sample_rate == 48 * 1000);
+      assert (channels == 1 || channels == 2);
+      // CBR
+      m_opus_encoder.vbr (false);
+      m_opus_encoder.bitrate (bitrate);
+      m_opus_encoder.signal (Opus::Signal::music);
+      //m_celt_encoder.vbr_rate = bitrate
+      //m_celt_encoder.prediction_request = 0
+      m_consume = std::thread {[this] ()
+                               {
+                                 this->consume();
+                               }};
     }
-                            };
-  }
-
-  Mumble2Mumble::~Mumble2Mumble ()
-  {
-    // kill_threads
-  }
-
-  void Mumble2Mumble::process_udp_tunnel (const MumbleProto::UDPTunnel &message)
+    void process_udp_tunnel (const MumbleProto::UDPTunnel &message);
+    void consume ();
+  };
+  
+  void Mumble2Mumble::Impl::process_udp_tunnel (const MumbleProto::UDPTunnel &message)
   {
     std::lock_guard <std::mutex> lock (m_pds_lock);
     auto packetstr = message.packet ();
@@ -87,36 +103,53 @@ m_celt_encoder (sample_rate, sample_rate / 100, channels, [bitrate / 6800, 127].
       }
   }
 
-  std::list<uint32_t> Mumble2Mumble::getspeakers ()
+  Mumble2Mumble::Mumble2Mumble (const Aither::Log &log, Codec codec,
+                                Connection &conn,
+                                size_t sample_rate,
+                                size_t channels, size_t bitrate)
+    : pimpl (std::make_unique<Impl> (log, codec, conn, sample_rate, channels, bitrate))
+  {
+  }
+
+  Mumble2Mumble::~Mumble2Mumble ()
+  {
+  }
+
+  void Mumble2Mumble::process_udp_tunnel (const MumbleProto::UDPTunnel &message)
+  {
+    pimpl->process_udp_tunnel (message);
+  }
+
+  std::list<uint32_t> Mumble2Mumble::get_users ()
   {
     std::list<uint32_t> list;
-    for (const auto& pair : m_queues)
+    for (const auto& pair : pimpl->m_queues)
       {
         list.push_back (pair.first);
       }
     return list;
   }
 
-  std::vector<int16_t> Mumble2Mumble::getframe (uint32_t speaker)
+  std::vector<int16_t> Mumble2Mumble::get_frame (uint32_t user_id)
   {
-    auto queue = m_queues.at (speaker);
+    auto queue = pimpl->m_queues.at (user_id);
     auto frame = queue.front ();
     queue.pop ();
     return frame;
   }
 
-  size_t Mumble2Mumble::getsize (uint32_t speaker)
+  size_t Mumble2Mumble::get_size (uint32_t user_id)
   {
-    return m_queues.at (speaker).size ();
+    return pimpl->m_queues.at (user_id).size ();
   }
 
   void Mumble2Mumble::produce (const std::vector<int16_t> &frame)
   {
     std::copy (std::begin (frame), std::end (frame),
-               std::back_inserter (m_rawaudio));
+               std::back_inserter (pimpl->m_rawaudio));
   }
 
-  void Mumble2Mumble::consume ()
+  void Mumble2Mumble::Impl::consume ()
   {
     auto num_frames = 0;
     switch (m_codec)
